a1b3e1df51f5cad14cae322374b6ca3dab701baa
Sumukh Remala <84765168+sumukherror404@users.noreply.github.com>
2021-11-25 16:57:21 +0530

    Create squareroot.java

---
+++ searching/binary search/squareroot.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;//importing the Scanner class from java.util package
+public class Main{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int n;
+        System.out.println("Enter the a number to check the squareroot:");
+        n=input.nextInt();
+        int value=n,res;
+        while(true){
+            res=Math.abs(value*value - n);
+            if(res<=0.1){
+                break;
+            }
+            else if(res>0.1){
+                value--;
+            }
+            else{
+                value++;
+            }
+        }
+        System.out.println(value);
+    }
+}
