caa7bb17ea0739676b594e150f70109d7f05cbd8
Sumukh Remala <84765168+sumukherror404@users.noreply.github.com>
2021-11-25 15:11:11 +0530

    Create counting_occurences_of_a_key.java
    The code demonstrates the implementation of linear search algorithm to count the number of occurrences of a key element in the given array

---
+++ searching/linear search/counting_occurences_of_a_key.java
@@ -0,0 +1,29 @@
+/*linear search algorithm to find the number of occurences of a particular element in the given list of numbers*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int n,key;
+        System.out.print("enter the size of the array:");
+        n=input.nextInt();
+        int[] a = new int[n];
+        System.out.print("enter the elements in the array:");
+        for(int i=0;i<n;i++){
+            a[i]=input.nextInt();
+        }
+        System.out.print("Enter the key element to be searched:");
+        key=input.nextInt();
+        int i,count=0;
+        for(i=0;i<n;i++){
+            if(a[i]==key){
+                count++;
+            }
+        }
+        if(count==0){
+            System.out.println("The key element is not found in the given array");
+        }
+        else{
+            System.out.println("The key element occured "+count+" times in the array");
+        }
+    }
+}
