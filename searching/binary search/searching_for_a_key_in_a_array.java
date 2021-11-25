ea405e9d42c3eafa1c84e97c7a290fea09042b4d
Sumukh Remala <84765168+sumukherror404@users.noreply.github.com>
2021-11-25 16:12:10 +0530

    Update searching_for_a_key_in_a_array.java
    This code demonstrates the operation of searching of a key element in the given array using the binary search algorithm.
    The main requirement of the binary search algorithms is to get the list of elements or the array in sorted order only.
    This algorithms fails in the case of unsorted data

--- searching/binary search/searching_for_a_key_in_a_array.java
+++ searching/binary search/searching_for_a_key_in_a_array.java
@@ -1,1 +1,38 @@
-
+/*to search for a key element in the array using binary search*/
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int n,key;
+        System.out.print("Enter the size of the array:");
+        n=input.nextInt();
+        int[] a=new int[n];
+        System.out.print("Enter the array elements in sorted order:");
+        for(int i=0;i<n;i++){
+            a[i]=input.nextInt();
+        }
+        System.out.print("Enter the key element to be searched:");
+        key=input.nextInt();
+        int start=0,end=n-1,mid=(n-1)/2;
+        boolean found=false;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(a[mid]==key){
+                found=true;
+                break;
+            }
+            else if(a[mid]>key){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        if(found){
+            System.out.println("The given key element is found at "+ mid +" index");
+        }
+        else{
+            System.out.println("The given key element is not found in the given array");
+        }
+    }
+}
